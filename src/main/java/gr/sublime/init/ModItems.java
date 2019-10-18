0e0c0ba5cc22f6106f988834be9721d50279e2dc
Grant <grantranda@gmail.com>
2019-10-18 14:36:18 -0400

    Added Niobium tools & armor.

--- src/main/java/gr/sublime/init/ModItems.java
+++ src/main/java/gr/sublime/init/ModItems.java
@@ -15,18 +15,19 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemDoor;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 
     // Armor materials
-    public static final ItemArmor.ArmorMaterial ARMOR_IRIDIUM = EnumHelper.addArmorMaterial("armor_iridium", Reference.ID + ":iridium", 30, new int[]{3, 5, 6, 3}, 13, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 2.5f);
-    public static final ItemArmor.ArmorMaterial ARMOR_TUNGSTEN = EnumHelper.addArmorMaterial("armor_tungsten", Reference.ID + ":tungsten", 25, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 3.0f);
+    public static final ItemArmor.ArmorMaterial ARMOR_IRIDIUM = EnumHelper.addArmorMaterial("armor_iridium", Reference.ID + ":iridium", 30, new int[]{3, 5, 7, 3}, 13, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 3.0f);
+    public static final ItemArmor.ArmorMaterial ARMOR_NIOBIUM = EnumHelper.addArmorMaterial("armor_niobium", Reference.ID + ":niobium", 20, new int[]{2, 5, 7, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 1.0f);
+    public static final ItemArmor.ArmorMaterial ARMOR_TUNGSTEN = EnumHelper.addArmorMaterial("armor_tungsten", Reference.ID + ":tungsten", 25, new int[]{3, 6, 6, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 2.5f);
 
     // Tool materials
     public static final Item.ToolMaterial TOOL_IRIDIUM = EnumHelper.addToolMaterial("tool_iridium", 3, 1360, 13.0f, 3.5f, 18);
+    public static final Item.ToolMaterial TOOL_NIOBIUM = EnumHelper.addToolMaterial("tool_niobium", 2, 480, 7.0f, 2.5f, 17);
     public static final Item.ToolMaterial TOOL_TUNGSTEN = EnumHelper.addToolMaterial("tool_tungsten", 2, 912, 9.0f, 3.0f, 16);
 
     // Misc
@@ -43,6 +44,10 @@ public class ModItems {
     public static final ArmorBase IRIDIUM_CHESTPLATE = new ArmorBase("iridium_chestplate", ARMOR_IRIDIUM, 5, EntityEquipmentSlot.CHEST, IRIDIUM);
     public static final ArmorBase IRIDIUM_LEGGINGS = new ArmorBase("iridium_leggings", ARMOR_IRIDIUM, 5, EntityEquipmentSlot.LEGS, IRIDIUM);
     public static final ArmorBase IRIDIUM_BOOTS = new ArmorBase("iridium_boots", ARMOR_IRIDIUM, 5, EntityEquipmentSlot.FEET, IRIDIUM);
+    public static final ArmorBase NIOBIUM_HELMET = new ArmorBase("niobium_helmet", ARMOR_NIOBIUM, 5, EntityEquipmentSlot.HEAD, NIOBIUM_INGOT);
+    public static final ArmorBase NIOBIUM_CHESTPLATE = new ArmorBase("niobium_chestplate", ARMOR_NIOBIUM, 5, EntityEquipmentSlot.CHEST, NIOBIUM_INGOT);
+    public static final ArmorBase NIOBIUM_LEGGINGS = new ArmorBase("niobium_leggings", ARMOR_NIOBIUM, 5, EntityEquipmentSlot.LEGS, NIOBIUM_INGOT);
+    public static final ArmorBase NIOBIUM_BOOTS = new ArmorBase("niobium_boots", ARMOR_NIOBIUM, 5, EntityEquipmentSlot.FEET, NIOBIUM_INGOT);
     public static final ArmorTungsten TUNGSTEN_HELMET = new ArmorTungsten("tungsten_helmet", 5, EntityEquipmentSlot.HEAD);
     public static final ArmorTungsten TUNGSTEN_CHESTPLATE = new ArmorTungsten("tungsten_chestplate", 5, EntityEquipmentSlot.CHEST);
     public static final ArmorTungsten TUNGSTEN_LEGGINGS = new ArmorTungsten("tungsten_leggings", 5, EntityEquipmentSlot.LEGS);
@@ -54,6 +59,11 @@ public class ModItems {
     public static final ToolShovel IRIDIUM_SHOVEL = new ToolShovel("iridium_shovel", TOOL_IRIDIUM, IRIDIUM);
     public static final ToolHoe IRIDIUM_HOE = new ToolHoe("iridium_hoe", TOOL_IRIDIUM, IRIDIUM);
     public static final ToolSword IRIDIUM_SWORD = new ToolSword("iridium_sword", TOOL_IRIDIUM, IRIDIUM);
+    public static final ToolPickaxe NIOBIUM_PICKAXE = new ToolPickaxe("niobium_pickaxe", TOOL_NIOBIUM, NIOBIUM_INGOT);
+    public static final ToolAxe NIOBIUM_AXE = new ToolAxe("niobium_axe", TOOL_NIOBIUM, NIOBIUM_INGOT);
+    public static final ToolShovel NIOBIUM_SHOVEL = new ToolShovel("niobium_shovel", TOOL_NIOBIUM, NIOBIUM_INGOT);
+    public static final ToolHoe NIOBIUM_HOE = new ToolHoe("niobium_hoe", TOOL_NIOBIUM, NIOBIUM_INGOT);
+    public static final ToolSword NIOBIUM_SWORD = new ToolSword("niobium_sword", TOOL_NIOBIUM, NIOBIUM_INGOT);
     public static final ToolPickaxe TUNGSTEN_PICKAXE = new ToolPickaxe("tungsten_pickaxe", TOOL_TUNGSTEN, TUNGSTEN_INGOT);
     public static final ToolAxe TUNGSTEN_AXE = new ToolAxe("tungsten_axe", TOOL_TUNGSTEN, TUNGSTEN_INGOT);
     public static final ToolShovel TUNGSTEN_SHOVEL = new ToolShovel("tungsten_shovel", TOOL_TUNGSTEN, TUNGSTEN_INGOT);
@@ -73,6 +83,16 @@ public class ModItems {
                 IRIDIUM_CHESTPLATE,
                 IRIDIUM_LEGGINGS,
                 IRIDIUM_BOOTS,
+                NIOBIUM_INGOT,
+                NIOBIUM_PICKAXE,
+                NIOBIUM_AXE,
+                NIOBIUM_SHOVEL,
+                NIOBIUM_HOE,
+                NIOBIUM_SWORD,
+                NIOBIUM_HELMET,
+                NIOBIUM_CHESTPLATE,
+                NIOBIUM_LEGGINGS,
+                NIOBIUM_BOOTS,
                 TUNGSTEN_INGOT,
                 TUNGSTEN_ROD,
                 TUNGSTEN_PICKAXE,
@@ -84,7 +104,6 @@ public class ModItems {
                 TUNGSTEN_CHESTPLATE,
                 TUNGSTEN_LEGGINGS,
                 TUNGSTEN_BOOTS,
-                NIOBIUM_INGOT,
                 DOOR_BLOODLESS_ELM,
                 DOOR_HANAMI
         );
@@ -103,6 +122,16 @@ public class ModItems {
                 IRIDIUM_CHESTPLATE,
                 IRIDIUM_LEGGINGS,
                 IRIDIUM_BOOTS,
+                NIOBIUM_INGOT,
+                NIOBIUM_PICKAXE,
+                NIOBIUM_AXE,
+                NIOBIUM_SHOVEL,
+                NIOBIUM_HOE,
+                NIOBIUM_SWORD,
+                NIOBIUM_HELMET,
+                NIOBIUM_CHESTPLATE,
+                NIOBIUM_LEGGINGS,
+                NIOBIUM_BOOTS,
                 TUNGSTEN_INGOT,
                 TUNGSTEN_ROD,
                 TUNGSTEN_PICKAXE,
@@ -114,7 +143,6 @@ public class ModItems {
                 TUNGSTEN_CHESTPLATE,
                 TUNGSTEN_LEGGINGS,
                 TUNGSTEN_BOOTS,
-                NIOBIUM_INGOT,
                 DOOR_BLOODLESS_ELM,
                 DOOR_HANAMI
         );
