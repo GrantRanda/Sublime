3807e6d424359697e161f8f09d25a6507e11b986
Grant <grantranda@gmail.com>
2020-02-16 15:35:26 -0500

    Updated version to 1.2

--- src/main/java/gr/sublime/util/Reference.java
+++ src/main/java/gr/sublime/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String ID = "sublime";
     public static final String NAME = "Sublime";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     public static final int GUI_NIOBIUM_CHEST = 1;
     public static final int GUI_TUNGSTEN_FURNACE = 2;
